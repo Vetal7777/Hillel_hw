38f0298bdb1e4e33930d5001d97bf71cba920355
Vitalii <jobschildren@gmail.com>
2022-04-26 16:45:38 +0300

    commit

--- script.js
+++ script.js
@@ -1,56 +1,46 @@
-const todoContainerNameE = getE('list-container');
-//Наш todo
-const inpNameE = getE('inp-name');
+const todoContainerE = getE('list-container');
+//Наша Todo phone поле с результатом
+const reminderElements = document.querySelectorAll('#reminder-one');
+const inpE = getE('inp');
 //Нащ input name
-const inpSurnameE = getE('inp-surname');
-//Нащ input surname
-const inpPhoneE = getE('inp-phone');
-//Нащ input phone
 const btnE = getE('btn');
 //Наша кнопка
-const SampleE = getE('sample')
+const SampleE = (reminder) =>
+    `<div id="reminder-one">
+        ${reminder}
+        <span id="exit">x</span>
+    </div>`;
 //Наш шаблон
 
 btnE.addEventListener('click',onAddTodo);
 //При клике запуская функцию onAddTodo
+todoContainerE.addEventListener("click", onClickDo);
+//При клике на крестик запускается функция onClickDeleteReminder
 
 function onAddTodo(){
-    const name = inpNameE.value;
+    const inp = inpE.value;
     //name - содержимое inpNameE
-    const surname = inpSurnameE.value;
-    //surname - содержимое inpSurnameE
-    const phone = inpPhoneE.value;
-    //phone - содержимое inpPhoneE
-    if(
-        !name.trim()
-        || !surname.trim()
-        || !phone.trim()
-        || !Number(phone)
-    ){
-        alert('Please add some text to todo');
-        clearValue(inpPhoneE);
-        inpPhoneE.focus();
-        return;
-    }
+    validate(inpE);
     //Условие если пустой один из input
-    const todoE = createToDoE(name,surname,phone);
+    const resultE = SampleE(inp);
     //Константа ...E - это теперь наш HTML который мы создали с
     //помощью функции
-    addElement(todoE,todoContainerNameE);
+    addElement(resultE,todoContainerE);
     //Добавляем наш элемент в наш ul
-    clearValue(inpNameE);
-    clearValue(inpSurnameE);
-    clearValue(inpPhoneE);
+    clearValue(inpE);
 //    Чистим все
-    inpNameE.focus();
+    inpE.focus();
 }
 
 function addElement(elem,container){
     container.innerHTML += elem;
 }
+//Добовляем новый элемент в контейнер
+
 function clearValue(inputE){
     inputE.value = '';
 }
+//Чистит элемент
 
 function getE(id){
     return document.getElementById(`${id}`);
@@ -58,13 +48,25 @@ function getE(id){
 //My func
 //get element
 
-function createToDoE(Name,Surname,Phone){
-    const result = SampleE.innerHTML
-        .replace(`{{Name}}`,Name)
-        .replace(`{{Surname}}`,Surname)
-        .replace(`{{Phone}}`,Phone);
-    //ставит вместо элементов который мы указали - значения input
-    return result;
+function validate(input){
+    if(!(input.value).trim()){
+        alert('Please add some text to todo');
+        input.focus();
+    }
+    //Условие если пустой один из input
 }
-//My func
-//get HTML
+//Валидация input
+
+function onClickDo(event) {
+    if (event.target.id === "exit") {
+        //если id обьекта на который мы нажали = exit то происходит следующее
+        const item = event.target.closest("#reminder-one");
+        //переменная item = все html элемента до родителя с классом .table-items-wrapper
+        item.remove();
+        //Удаляем данную переменную
+    }else if (event.target.id === 'reminder-one'){
+        //если id обьекта на который мы нажали = list-containe то происходит следующее
+        const item = event.target.closest("#reminder-one");
+        item.classList.toggle('yellow');
+    }
+}
