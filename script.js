5fdce7b43830c514756aafa98aebe457b3fb3678
Vitalii <jobschildren@gmail.com>
2022-05-02 18:49:50 +0300

    commit

--- script.js
+++ script.js
@@ -1,179 +1,131 @@
-const todoContainerE = getE('list-container');
-//Наша Todo phone поле с результатом
-const inpE = getE('inp');
-//Нащ input name
-const btnE = getE('btn');
-//Наша кнопка
-const errorE = document
-    .querySelector('.error');
-//контейнер ошибки
-const SampleE = (reminder) =>
-    `<div class="reminder-one">
-        <span class="condition">
-            <span></span>
-        </span>
-        <p>${reminder}</p>
-        <span class="exit">
-            <span></span>
-            <span></span>
-        </span>
-    </div>`;
-//Наш шаблон
+const SIZES = {
+    SMALL:{
+        price: 50,
+        calories: 20,
+    },
+    MIDDLE:{
+        price: 75,
+        calories: 30,
+    },
+    BIG:{
+        price: 100,
+        calories: 40,
+    },
+};
 
-btnE.disabled = true;
-//Поумолчанию кнопка не активна
-inpE.addEventListener('keyup',forInput);
-//При отпускании клавиши запускается функция validateTodo
-btnE.addEventListener('click',onAddTodo);
-//При клике запуская функцию onAddTodo
-todoContainerE.addEventListener("click", onClickDo);
-//При клике на крестик запускается функция onClickDeleteReminder
-inpE.focus();
-//Фокус ставим на input
+const TOPINGS = {
+    CHEESE:{
+        price: 10,
+        calories: 20,
+    },
+    SALATE:{
+        price: 20,
+        calories: 5,
+    },
+    POTATOE:{
+        price: 15,
+        calories: 10,
+    },
+    SEASONING:{
+        price: 15,
+        calories: 0,
+    },
+    MAYONEZ:{
+        price: 20,
+        calories: 5,
+    },
+};
 
-function onAddTodo(){
-    const inp = inpE.value;
-    //name - содержимое inpNameE
-    if(!(inpE.value).trim()){
-        alert('Please add some text to todo');
-        inpE.focus();
-        return;
-    }
-    //Условие если пустой один из input
-    const resultE = SampleE(inp);
-    //Константа ...E - это теперь наш HTML который мы создали с
-    //помощью функции
-    addElement(resultE,todoContainerE);
-    //Добавляем наш элемент в наш ul
-    clearValue(inpE);
-//    Чистим все
-    inpE.focus();
-//    Фокус ставим на input
-    btnE.disabled = true;
+function Hamburger(){
+    this.topings = [];
 }
-
-function addElement(elem,container){
-    container.innerHTML += elem;
-}
-//Добовляем новый элемент в контейнер
-
-function clearValue(inputE){
-    inputE.value = '';
-}
-//Чистит элемент
-
-function getE(id){
-    return document.getElementById(`${id}`);
-}
-//My func
-//get element
-
-function onClickDo(event) {
-    if (event.target.className === "exit") {
-        //если id обьекта на который мы нажали = exit то происходит следующее
-        const item = event.target.closest(".reminder-one");
-        //переменная item = все html элемента до родителя с классом .table-items-wrapper
-        item.remove();
-        //Удаляем данную переменную
+const doubleCheeseburger = new Hamburger();
+Hamburger.prototype.askBurgerSize = function (){
+    const sizeOptions = Object.keys(SIZES);
+    console.log(`Ask size of burger one of ${sizeOptions}`);
+    let userSize = prompt(`What size of burger do you want?(${sizeOptions})`,'SMALL');
+    let result = this.validateSize(userSize,sizeOptions);
+    if (result){
+        console.log(`User's size is possible`);
+        this.size = userSize;
+        console.log(`Burger size ${this.size}`);
+        this.price = Object.values(SIZES)[sizeOptions.indexOf(this.size)].price;
+        console.log(`Burger price ${this.price}$`);
+        this.calories = Object.values(SIZES)[sizeOptions.indexOf(this.size)].calories;
+        console.log(`Burger calories ${this.calories}cal`);
     }
-    if(event.target.closest('.reminder-one')){
-        const item = event.target.closest(".reminder-one");
-        item.classList.toggle('yellow');
+};
+//Ask about size of burger
+//Add:
+// - size
+// - price
+// - calories
+Hamburger.prototype.validateSize = function (userSize,sizeOptions){
+    console.log(`Validate user's size`);
+    if (userSize === null
+        || !userSize.trim()
+        || !sizeOptions.includes(userSize)){
+        console.log(`ERROR , size is not possible.`)
+        this.askBurgerSize();
+        return false;
     }
-    //Добавляет класс yellow при нажатии на todo
-    deleteAllSelected(event);
-    toggleActiveClassBTNCloseAllSelected(event);
+    return true;
 }
-//Удаление при клике на соответвующую кнопку
-//Добавляет класс yellow при нажатии на todo
-
-function validateTodo(e){
-    if(!e.target.value.trim()){
-        //если элемент пустой или одни пробелы
-        errorE.innerText = '';
-        //то текст внутри errorE пустой
-        btnE.disabled = true;
-        //Кнопка не активна
-        if([...errorE.classList].includes('active')){
-            errorE.classList.toggle('active');
-        }
-        //Если у элемента errorE есть класс active
-        //то мы его удаляем
-        return;
-        //Выходим из функции
-    }
-    if(e.target.value.trim().length <= 3){
-        //Если текст меньше или равен трем символам
-        errorE.innerText = 'Error , lenght should be > 3';
-        //То выскакивает ошибка
-        btnE.disabled = true;
-        //Кнопка не активна
-        if(![...errorE.classList].includes('active')){
-            errorE.classList.toggle('active');
-        }
-        //Если у элемента errorE нету класса active
-        //то мы его добавляем
-        return;
-        //Выходим
-    }
-    if([...errorE.classList].includes('active')){
-        errorE.classList.toggle('active');
-    }
-    //Если у элемента errorE есть класс active
-    //то мы его удаляем
-    errorE.innerText = '';
-    //В других случиях поле ошибки пустое
-    btn.disabled = false;
-    //Кнопка активна
-}
-//Валидация Todo
-
-function enterInput(e){
-    if(btnE.disabled === false
-        && e.keyCode === 13){
-        onAddTodo();
+//Validate user's answer about size of burger
+Hamburger.prototype.askAboutAddToppings = function (){
+    console.log(`Ask user ,does he want add topping or no`);
+    let addToppings = confirm(`Do yo want add toppings to your burger?`);
+    if(addToppings){
+        console.log(`Add`);
+        this.askTypeOfTopping();
+    }else{
+        console.log(`Don't add`);
+        this.answerOnTheEnd();
     }
 }
-//При нажатии Enter запускается функция onAddTodo
-
-function shiftBackspaceInput(e){
-    if(e.keyCode === 8 && e.shiftKey === true){
-       inpE.value = '';
-    }
-}
-//Удаляет при сочетании клавиш shift + backspace
-
-function forInput(e){
-    shiftBackspaceInput(e);
-    enterInput(e);
-    validateTodo(e);
-}
-//Функции для input
-
-function toggleActiveClassBTNCloseAllSelected(event){
-    if(event.target.closest('.reminder-one')
-        && document.querySelectorAll('.yellow').length === 0){
-        document.getElementById('exit-all').classList.toggle('active');
-    }
-    let test = [...document
-        .getElementById('exit-all').classList];
-    if(event.target.closest('.reminder-one')
-        && document
-            .querySelectorAll('.yellow')
-            .length === 1
-        && !test.includes('active')){
-        document.getElementById('exit-all').classList.toggle('active');
-    }
-}
-//Функция которая решает удалять или добавлять класс active кнопке exit-all
-
-function deleteAllSelected(event){
-    if(event.target.className === 'active'){
-        const deleted = document
-            .querySelectorAll('.yellow');
-        deleted.forEach((elem) => elem.remove());
-        document.getElementById('exit-all').classList.toggle('active');
+//Ask user about add toppings for burger
+Hamburger.prototype.askTypeOfTopping = function (){
+    const typesToppings = Object.keys(TOPINGS);
+    console.log(`Ask type of topping from ${typesToppings}`);
+    const userTopping = prompt(`What topping do you want?${typesToppings}`,`CHEESE`);
+    const result = this.validateTopping(userTopping,typesToppings);
+    if(result){
+        console.log(`User's topping is possible`);
+        this.topings.push(userTopping);
+        console.log(`Add topping ${userTopping}`);
+        this.price += Object.values(TOPINGS)[typesToppings.indexOf(userTopping)].price;
+        console.log(`Plus to price toppings cost ${Object.values(TOPINGS)[typesToppings.indexOf(userTopping)].price}$`);
+        this.calories += Object.values(TOPINGS)[typesToppings.indexOf(userTopping)].calories;
+        console.log(`Plus to calories toppings calories ${Object.values(TOPINGS)[typesToppings.indexOf(userTopping)].calories}cal`);
+        this.askAboutAddToppings();
     }
 
 }
-//Функция которая удаляет обозначеные туду
+//Ask what type of toppings user want
+//Plus:
+// - price
+// - calories
+Hamburger.prototype.validateTopping = function (userTopping,toppingOptions){
+    console.log(`Validate user's topping`);
+    if (userTopping === null
+        || !userTopping.trim()
+        || !toppingOptions.includes(userTopping)){
+        console.log(`ERROR , topping is not possible.`)
+        this.askTypeOfTopping();
+        return false;
+    }
+    return true;
+}
+//Validate user's answer about topping of burger
+Hamburger.prototype.answerOnTheEnd = function (){
+    console.log(`Write user's order`);
+    alert(`You order ${this.size.toLowerCase()} burger 
+    Price ${this.price}$,
+    Calories ${this.calories}cal`);
+}
+Hamburger.prototype.playAllFunc = function (){
+    this.askBurgerSize();
+    this.askAboutAddToppings();
+}
+doubleCheeseburger.playAllFunc();
+console.log(doubleCheeseburger);
